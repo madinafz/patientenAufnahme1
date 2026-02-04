06ed94addee016674e993b2341b36e9d4cd52a76
madinafz <madina.fayz@edu.fh-joanneum.at>
2026-02-04 20:33:07 +0100

    commit 4

---
+++ src/main/java/org/example/DB.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+       private static final String URL =
+            "jdbc:mysql://10.25.2.145:3306/24abfa?useSSL=false&serverTimezone=UTC";
+
+        private static final String USER = "24fama";
+        private static final String PASS = "geb24";
+
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(URL, USER, PASS);
+        }
+
+
+}
